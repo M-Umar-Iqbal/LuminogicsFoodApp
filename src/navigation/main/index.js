ac4c2c29ff7c924c448633f29844397cc432ed8e
Umar <umardottiqbal@gmail.com>
2022-08-14 13:49:27 +0500

    UmarDevelopment-02:Creating Bottom Navigation

--- src/navigation/main/index.js
+++ src/navigation/main/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import {HomeScreen} from '../../screens/main/Home';
+import {View, Text, SafeAreaView} from 'react-native';
 const Navigation = () => {
   const Tab = createBottomTabNavigator();
   return (
-    <View>
-      <Text>hello</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Navigation Page</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 export default Navigation;
